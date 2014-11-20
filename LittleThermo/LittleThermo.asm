406fb6aaf19317cb44804fa00d38475b6066e1f3
vmouse <vlad.baginsky@gmail.com>
2014-11-20 12:50:40 +0400

    test

--- LittleThermo/LittleThermo.asm
+++ LittleThermo/LittleThermo.asm
@@ -7,8 +7,8 @@
 
  .include "avr.inc"
 
-.EQU	led1 = PB1
-.EQU	led2 = PB0
+.EQU	led1 = PB0
+.EQU	led2 = PB1
 .EQU	led3 = PB2
 
 //#define	F_CPU 8000000
@@ -19,9 +19,9 @@
 
 .dseg
 .ORG	RAMTOP
-.EQU	MaxInputSize	=	32 
-LineBuf:.byte	MaxInputSize	;Command line characters buffer 
-ByteBuf:.byte	MaxInputSize/2  ;Command line bytes buffer
+;.EQU	MaxInputSize	=	32
+;LineBuf:.byte	MaxInputSize	;Command line characters buffer 
+;ByteBuf:.byte	MaxInputSize/2  ;Command line bytes buffer
 
 ;----------------------------------------------------------;
 ; Program code area
@@ -97,7 +97,7 @@ ReadOneWire:
 	OW_cmd	0xCC			; адресуемся ко всему
 	OW_cmd	0x44			; dallas - measure
 
-	ldi		r16, 100
+	ldi		r16, 200
 	rcall	WaitMiliseconds
 
 	rcall	OWReset
@@ -114,32 +114,43 @@ read_temp_onewire:			; читаем и преобразуем температуру
 	rcall	OWReadByte		; старший байт LO - старшие разряды целых
 	cpi		r16, 0x08			; проверям на отрицательность
 	brlo	temp_up_zerro	; выше нуля - уходим
-	; тут надо сделать преобразование из дополненного кода
-	andi	r16, 0x07		; пока просто отсечем биты знака (что не правильно)
+//; тут надо сделать преобразование из дополненного кода, но пока упростим
+	andi	r16, 0x07		; просто отсечем биты знака (что не правильно) и возьмем целую часть
 temp_up_zerro:
 	andi	r17, 0xf0		; HI(R17) = младшие знаки температуры
 	or		r17, r16		; HI(R17)- младшие, LO(R17) - старшие
 	swap	r17				; теперь все на своих местах
 
-	sbi		PORTB, led1		; turn off
-	sbi		PORTB, led2		; turn off
-	sbi		PORTB, led3		; turn off
+	sbi		PORTB, led1		; turn off blue
+	sbi		PORTB, led2		; turn off green
+	sbi		PORTB, led3		; turn off red
 
-	cpi		r17, 50
-	brlo	less50
-	cbi		PORTB, led3		; hi temp
+	cpi		r17, 39
+	brlo	less39
+
+	in		r16, PORTB		; ultra high
+	ldi		r18, (1<<led3)
+	eor		r16, r18
+	out		PORTB, r16		; flash bit led3
 	ret
-less50:
-	cpi		r17, 40
-	brlo	less40	
-	cbi		PORTB, led2
+less39:
+	cpi		r17, 38
+	brlo	less38
+	cbi		PORTB, led3		; high
 	ret
-less40:
-	cpi		r17, 30
-	brlo	less30	
-	cbi		PORTB, led1
+less38:
+	cpi		r17, 37
+	brlo	less37	
+	cbi		PORTB, led2		; raised
+	cbi		PORTB, led3		; raised
 	ret
-less30:
+less37:
+	cpi		r17, 36
+	brlo	less36	
+	cbi		PORTB, led2		; normal
+	ret
+less36:
+	cbi		PORTB, led1		; low
 	ret
 
 .include "1-wire.asm"
